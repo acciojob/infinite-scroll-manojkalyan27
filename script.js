b8f6cf08006c2e78326ccb183716089ae05b4032
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-03 16:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const infiList = document.querySelector('#infi-list');
+let countItem = 1;
+function addList(count){
+  for(let i=1;i<=count;++i){
+    const list = document.createElement('li');
+    list.textContent =`list item${countItem++}`
+    infiList.appendChild(list);
+  }
+}
 
+addList(10);
+
+infiList.addEventListener('scroll', ()=>{
+  if(infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight -1 ){
+    addList(2);
+  }
+});
+
+// scrollTop: How much has been scrolled vertically.
+// clientHeight: The visible height of the container.
+// scrollHeight: The total height of the content.
